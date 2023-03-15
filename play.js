c5db6fe286ac46a84e67fb72cdeef62d32d54057
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-15 14:13:29 -0600

    Refactored client.js and play.js files

--- play.js
+++ play.js
@@ -1,17 +1,4 @@
 const { connect } = require("./client");
 
-// establishes a connection with the game server
-const conn = connect();
-// interpret incoming data as text
-conn.setEncoding("utf8");
-
-conn.on("data", (data) => {
-  console.log("Server says:", data);
-});
-
-conn.on("connect", () => {
-  console.log("Ready to play");
-});
-
 console.log("Connecting ...");
 connect();
