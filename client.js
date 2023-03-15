2c54a449f89fac8a58f1cf5dfec84e4867a9e9fb
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-15 17:54:15 -0600

    Separated some hard coded code from client.js to constants.js

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require('./constants')
 
 const connect = () => {
 
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   conn.setEncoding("utf8");
